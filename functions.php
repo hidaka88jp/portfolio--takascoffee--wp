897b421b41594589a0dd52673ee258c47e3e4443
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-22 09:59:44 -0800

    chore: restructure theme files and add style enqueue functionality

--- functions.php
+++ functions.php
@@ -9,3 +9,14 @@ function takascoffee_theme_setup() {
 }
 
 add_action('after_setup_theme', 'takascoffee_theme_setup');
+
+function takascoffee_enqueue_styles() {
+  wp_enqueue_style(
+    'takascoffee-style',
+    get_stylesheet_uri(),
+    [],
+    filemtime(get_template_directory() . '/style.css')
+  );
+}
+
+add_action('wp_enqueue_scripts', 'takascoffee_enqueue_styles');
--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
-echo "<h1>Hello Taka's Coffee</h1>";
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<h1>Hello</h1>
+
+<?php wp_footer(); ?>
+</body>
+</html>
