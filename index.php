c1bb16f1386142d4c0e56051af6089078c9c3b43
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-22 11:00:14 -0800

    chore: update index.php structure

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
-<h1>Hello</h1>
+<main>
+  <?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <article>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+      </article>
+    <?php endwhile; ?>
+  <?php else : ?>
+    <p>No content found.</p>
+  <?php endif; ?>
+</main>
 
 <?php get_footer(); ?>
