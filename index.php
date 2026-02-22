fecea083efa4b2d2cb1553b6a273bffa7d4f5c89
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-22 10:06:39 -0800

    chore: create header and footer template files for theme structure

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+<?php get_header(); ?>
 
 <h1>Hello</h1>
 
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
