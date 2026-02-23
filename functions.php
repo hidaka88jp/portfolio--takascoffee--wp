66ba0f8f6cb07372e20bf56761eaf1b811e76960
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-23 13:20:42 -0800

    feat: add header styles and Google Fonts integration

--- functions.php
+++ functions.php
@@ -11,11 +11,20 @@ function takascoffee_theme_setup() {
 add_action('after_setup_theme', 'takascoffee_theme_setup');
 
 function takascoffee_enqueue_styles() {
+  // Enqueue the main stylesheet with a version number based on the file modification time for cache busting.
   wp_enqueue_style(
     'takascoffee-style',
-    get_stylesheet_uri(),
+    get_theme_file_uri('/assets/css/style.css'),
     [],
-    filemtime(get_template_directory() . '/style.css')
+    filemtime(get_template_directory() . '/assets/css/style.css')
+  );
+
+  // Enqueue Google Fonts.
+  wp_enqueue_style(
+    'takascoffee-google-fonts',
+    'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,700;1,700&display=swap',
+    [],
+    null
   );
 }
 
