4fabe62f0fc0ce847e3f0410d02703bfdd338cf2
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-26 13:04:05 -0800

    feat: add hero section to front page and update styles for responsiveness

--- front-page.php
+++ front-page.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 
 <main>
-  <p>Top Page</p>
+  <div class="hero">
+      <div class="hero__text">
+        <h1 class="hero__heading">Every great idea <br>begins with coffee.</h1>
+        <p class="hero__subheading">Even waiting for your coffee becomes a special moment.</p>
+      </div>
+    </div><!-- /.hero -->
 </main>
 
 <?php get_footer(); ?>
