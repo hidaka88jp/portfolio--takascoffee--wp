60f40c0a81fdce7db1d4427ca9e2beee5ffcc121
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-26 13:33:14 -0800

    feat: update front page layout and styles, add lead section with quote and link

--- front-page.php
+++ front-page.php
@@ -2,11 +2,22 @@
 
 <main>
   <div class="hero">
-      <div class="hero__text">
-        <h1 class="hero__heading">Every great idea <br>begins with coffee.</h1>
-        <p class="hero__subheading">Even waiting for your coffee becomes a special moment.</p>
-      </div>
-    </div><!-- /.hero -->
+    <div class="hero__text">
+      <h1 class="hero__heading">Every great idea <br>begins with coffee.</h1>
+      <p class="hero__subheading">Even waiting for your coffee becomes a special moment.</p>
+    </div>
+  </div><!-- /.hero -->
+  <div class="lead">
+    <p class="lead__quote">“Imagination can take you anywhere.”</p>
+    <p class="lead__sentence">
+      A line I found in a book I opened while waiting for my order.<br>
+      In the gentle flow of time, I remember the joy of letting my thoughts wander.<br>
+      And in moments like these, a good cup of coffee makes it even better.
+    </p>
+    <div class="lead__link-button-area">
+      <a href="./concept.html" class="link-button">CONCEPT</a>
+    </div>
+  </div><!-- /.lead -->
 </main>
 
 <?php get_footer(); ?>
