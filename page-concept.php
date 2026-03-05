4ed36104f04a5d36b40dcab67f2f8ec63aec18bb
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-03-05 12:01:14 -0800

    feat: add concept page template with header and footer structure

---
+++ page-concept.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main class="page content">
+
+  <h1><?php the_title(); ?></h1>
+
+  <div class="entry-content">
+    <?php the_content(); ?>
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
