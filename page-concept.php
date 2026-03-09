8b35d0cf98ffcb9a3219929dbee0656d867eeff9
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-03-09 09:43:26 -0700

    feat: restructure concept page to include dynamic sections with titles and images

--- page-concept.php
+++ page-concept.php
@@ -6,10 +6,47 @@
     <h1 class="page-hero__title"><?php the_title(); ?></h1>
   </div>
 
-  <div class="entry-content">
-    <?php the_content(); ?>
-  </div>
+  <?php
+    $title1 = get_field('section1_title');
+    $text1 = get_field('section1_text');
+    $image1 = get_field('section1_image');
+    $title2 = get_field('section2_title');
+    $text2 = get_field('section2_text');
+    $image2 = get_field('section2_image');
+  ?>
 
+  <section class="concept-section">
+
+    <div class="concept-section__item">
+      <div class="concept-section__text">
+        <h2><?php echo esc_html($title1); ?></h2>
+        <p><?php echo nl2br(esc_html($text1)); ?></p>
+      </div>
+      <div class="concept-section__image">
+        <?php if ($image1): ?>
+          <img
+            src="<?php echo esc_url($image1['url']); ?>"
+            alt="<?php echo esc_attr($image1['alt']); ?>"
+          >
+        <?php endif; ?>
+      </div>
+    </div>
+    <div class="concept-section__item concept-section__item--reverse">
+      <div class="concept-section__text">
+        <h2><?php echo esc_html($title2); ?></h2>
+        <p><?php echo nl2br(esc_html($text2)); ?></p>
+      </div>
+      <div class="concept-section__image">
+        <?php if ($image2): ?>
+          <img
+            src="<?php echo esc_url($image2['url']); ?>"
+            alt="<?php echo esc_attr($image2['alt']); ?>"
+          >
+        <?php endif; ?>
+      </div>
+    </div>
+
+  </section>
 </main>
 
 <?php get_footer(); ?>
