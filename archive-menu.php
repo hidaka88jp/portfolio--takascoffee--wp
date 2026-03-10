b11d449addc048e2a001254723911c2957d482b1
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-03-10 13:50:49 -0700

    feat: add menu page with dynamic menu items and styling

---
+++ archive-menu.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+<main class="page-content menu">
+  <div class="page-hero">
+    <h1 class="page-hero__title">Menu</h1>
+  </div>
+
+  <ul class="menu-list">
+    <?php
+      $categories = ['drink', 'food', 'dessert'];
+
+      foreach ($categories as $category) :
+        $query = new WP_Query([
+          'post_type' => 'menu',
+          'tax_query' => [
+            [
+              'taxonomy' => 'menu_category',
+              'field' => 'slug',
+              'terms' => $category,
+            ]
+          ]
+        ]);
+
+        if ($query->have_posts()) :
+    ?>
+          <?php while ($query->have_posts()) : $query->the_post(); ?>
+            <li class="menu-list__items">
+              <a href="<?php echo esc_url(get_permalink()); ?>" class="menu-list__link">
+                <?php if (has_post_thumbnail()) : ?>
+                  <?php the_post_thumbnail('medium'); ?>
+                <?php endif; ?>
+                <dl class="menu-list__item-desc">
+                  <dt class="menu-list__item-name"><?php the_title(); ?></dt>
+                  <dd class="menu-list__item-sentence"><?php the_excerpt(); ?></dd>
+                </dl>
+                <p class="menu-list__price">$<?php echo esc_html(get_field('price')); ?></p>
+                <?php
+                  $terms = get_the_terms( get_the_ID(), 'menu_category' );
+                  if ( $terms && ! is_wp_error( $terms ) ) :
+                ?>
+                  <p class="menu-list__category"><?php echo esc_html($terms[0]->name); ?></p>
+                <?php
+                  endif;
+                ?>
+              </a>
+            </li>
+          <?php endwhile; ?>
+    <?php
+        endif;
+        wp_reset_postdata();
+      endforeach;
+    ?>
+  </ul>
+</main>
+
+<?php get_footer(); ?>
