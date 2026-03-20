c27db883d565821410f91e57a1d8b289b873e47a
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-03-20 11:11:33 -0700

    refactor: Clean up whitespace and formatting in home.php

--- home.php
+++ home.php
@@ -1,42 +1,31 @@
 <?php get_header(); ?>
 
 <main class="page-content blog">
-
   <div class="page-hero">
     <h1 class="page-hero__title">Blog</h1>
   </div>
 
   <section class="blog-section">
-
     <?php if ( have_posts() ) : ?>
-
       <ul class="blog-list">
-
         <?php while ( have_posts() ) : the_post(); ?>
-
           <li class="blog-list__item">
             <article>
               <a href="<?php echo esc_url( get_permalink() ); ?>" class="blog-list__link">
-
                 <?php if ( has_post_thumbnail() ) : ?>
                   <?php the_post_thumbnail('large', [
                     'class' => 'blog-list__image'
                   ]); ?>
                 <?php endif; ?>
-
                 <div class="blog-list__desc">
                   <h2 class="blog-list__title"><?php the_title(); ?></h2>
                   <div class="blog-list__sentence"><?php the_excerpt(); ?></div>
                 </div>
-
               </a>
             </article>
           </li>
-
         <?php endwhile; ?>
-
       </ul>
-
       <div class="blog-section__pagination">
         <?php
           the_posts_pagination([
@@ -46,15 +35,10 @@
           ]);
         ?>
       </div>
-
     <?php else : ?>
-
       <p>No content found.</p>
-
     <?php endif; ?>
-    
   </section>
-
 </main>
 
 <?php get_footer(); ?>
